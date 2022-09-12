bf409a397e0bdb2529fb3eb56b942757c1ff0b72
vijju <vijju@localhost>
2022-09-12 20:52:10 +0530

    First commit from the code

---
+++ test-repository/src/test/java/io/spring/test/TestRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
